804c4bfa54a81c0690a1d021af93a3a5e69b1647
sumin <gosumin0710@naver.com>
2023-03-06 16:03:30 +0900

    home calender recycler part3 완료

--- app/src/main/java/com/umc/dodam/src/main/home/HomeFragment.kt
+++ app/src/main/java/com/umc/dodam/src/main/home/HomeFragment.kt
@@ -1,15 +1,24 @@
 package com.umc.dodam.src.main.home
 
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.umc.dodam.R
 import com.umc.dodam.databinding.FragmentHomeBinding
 import com.umc.dodam.src.main.home.homeStepRecycler.HomeStepAdapter
 import com.umc.dodam.src.main.home.homeStepRecycler.HomeStepItem
 import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.umc.dodam.src.main.home.homeCalenderRecycler.CalenderAdapter
+import java.time.LocalDate
+import java.time.Month
+import java.time.YearMonth
+import java.time.format.DateTimeFormatter
 
 
 class HomeFragment : Fragment() {
@@ -17,13 +26,18 @@ class HomeFragment : Fragment() {
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
 
+    lateinit var selectedDate: LocalDate;
+
     //home 화면의 단계 recycler 연결
     lateinit var homeStepAdapter: HomeStepAdapter
     val homeStepList = mutableListOf<HomeStepItem>()
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        //현재 날짜
+        selectedDate = LocalDate.now()
     }
 
     override fun onCreateView(
@@ -50,6 +64,7 @@ class HomeFragment : Fragment() {
         return view
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -60,6 +75,22 @@ class HomeFragment : Fragment() {
 
             writeScheduleDialog.show()
         }
+
+        //월 텍스트뷰 세팅
+        setMonthView();
+
+        //전월, 익월 버튼
+        binding.btnPrev.setOnClickListener(){
+            selectedDate = selectedDate.minusMonths(1)
+            setMonthView()
+        }
+        binding.btnNext.setOnClickListener(){
+            selectedDate = selectedDate.plusMonths(1)
+            setMonthView()
+        }
+
+        //달력 recycler 연결
+        val calenderRecyclerView : RecyclerView = binding.rvCalenderDay
     }
     // 프래그먼트가 destroy (파괴) 될때
     override fun onDestroyView() {
@@ -67,5 +98,51 @@ class HomeFragment : Fragment() {
         // onDestroyView 에서 binding class 인스턴스 참조를 정리해주어야 한다.
         _binding = null
         super.onDestroyView()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun monthFromDate(date:LocalDate) : String{
+        var formatter:DateTimeFormatter = DateTimeFormatter.ofPattern("MM")
+        return date.format(formatter)
+    }
+
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun setMonthView() {
+        // 년월 텍스트뷰 세팅
+        binding.tvCalenderMonth.setText(monthFromDate(selectedDate))
+
+        val dayList: ArrayList<String> = createDateArray(selectedDate)
+
+        //레이아웃 설정(열 7개)
+        binding.rvCalenderDay.layoutManager = GridLayoutManager(requireContext(), 7)
+
+        binding.rvCalenderDay.adapter = CalenderAdapter(dayList)
+    }
+
+    //날짜 생성 메소드
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createDateArray(date: LocalDate): ArrayList<String>{
+        val dayList: ArrayList<String> = arrayListOf()
+        val yearMonth: YearMonth = YearMonth.from(date)
+
+        //해당 월 마지막 날짜 가져오기
+        val lastDay: Int = yearMonth.lengthOfMonth();
+
+        //해당 월 첫번째 날 가져오기
+        val firstDay: LocalDate = selectedDate.withDayOfMonth(1);
+
+        //첫번째 날 요일 가져오기
+        val dayOfWeek: Int = firstDay.dayOfWeek.getValue()
+
+        for(i: Int in 1..41){
+            // 날짜가 없는 칸엔 빈 칸으로 채워넣기
+            if (i <= dayOfWeek || i > lastDay + dayOfWeek){
+                dayList.add("")
+            }else{
+                dayList.add((i-dayOfWeek).toString())
+            }
+        }
+        return dayList
     }
 }
